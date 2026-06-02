abf1c8130eb5063d791854276c51eba22d27b1b4
lailagabr <124739171+lailagabr@users.noreply.github.com>
2026-06-03 02:52:29 +0300

    Update environment.ts

--- Front-End/src/environments/environment.ts
+++ Front-End/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
-  signalRUrl: 'http://localhost:5000/Hub/chatHub'
+  apiUrl: 'http://localhost:5001/api',
+  signalRUrl: 'http://localhost:5001/Hub/chatHub'
 };
