f8bb0e46e71cea18de65005074d37d16c3913e67
Mohamed Tarek <mohamedtarekbadr047@gmail.com>
2026-06-06 20:40:32 +0300

    Add Email and IsVerified to VendorDetailsDTO
    Added Email and IsVerified properties to the VendorDetailsDTO class to support vendor contact information and verification status.

--- API/src/Application/DTOs/VendorDTOs/VendorDetailsDTO.cs
+++ API/src/Application/DTOs/VendorDTOs/VendorDetailsDTO.cs
@@ -10,7 +10,10 @@ namespace Application.DTOs.VendorDTOs
         public string BusinessName { get; set; }
         public decimal Rating { get; set; }
         public string Description { get; set; }
+        public string Email { get; set; }
+        public bool IsVerified { get; set; }
 
+        public string PhoneNumber { get; set; }
         public decimal YearsInBusiness { get; set; } 
         public decimal StartingPrice { get; set; }
         public ICollection<VendorRatingDTO> VendorRating { get; set; } = new List<VendorRatingDTO>();
